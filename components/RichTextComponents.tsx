8bd8bda2644ccbdbdedb20de6d76631ec32d2124
rurigrass <ruripg@gmail.com>
2023-04-24 22:27:09 +0200

    richtextcomponents

---
+++ components/RichTextComponents.tsx
@@ -0,0 +1,23 @@
+import urlFor from "@/lib/urlFor"
+import Image from "next/image"
+import Link from "next/link"
+
+
+export const RichTextComponents = () => {
+    types: {
+        image: ({value}: any) => {
+            return ()
+        }
+      },
+    
+      marks: {
+        link: ({children, value}) => {
+          const rel = !value.href.startsWith('/') ? 'noreferrer noopener' : undefined
+          return (
+            <a href={value.href} rel={rel}>
+              {children}
+            </a>
+          )
+        },
+      },
+}
